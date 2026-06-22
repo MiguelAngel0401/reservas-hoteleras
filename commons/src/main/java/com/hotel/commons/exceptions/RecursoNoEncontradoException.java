839e4e56b1a5c9dc5642313dfd2bdcdc1090b494
heric-hh <erhh.015@gmail.com>
2026-06-22 17:30:46 -0600

    Modulo de commons con configuracion inicial

---
+++ commons/src/main/java/com/hotel/commons/exceptions/RecursoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.hotel.commons.exceptions;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+    public RecursoNoEncontradoException(String message) {
+        super(message);
+    }
+}
+
---
+++ commons/src/main/java/com/hotel/commons/mappers/CommonMapper.java
@@ -0,0 +1,7 @@
+package com.hotel.commons.mappers;
+
+public interface CommonMapper<RQ, RS, E> {
+    E requestAEntidad(RQ request);
+    RS entidadAResponse(E entidad);
+}
+
