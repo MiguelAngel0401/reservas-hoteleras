7a66bbcd5f97945837d36e053459ac5b1cdb90e2
MiguelIslas <mislas.smartsoft@gmail.com>
2026-06-23 09:26:57 -0600

    Iniciando microoservicio habitaciones

---
+++ mcv-habitaciones/src/main/java/com/hotel/habitaciones/dto/HabitacionesResponse.java
@@ -0,0 +1,14 @@
+package com.hotel.habitaciones.dto;
+
+import java.math.BigDecimal;
+
+public record HabitacionesResponse(
+        Long id,
+        Integer numero,
+        String tipo,
+        BigDecimal precio,
+        Integer capacidad,
+        String estadoHabitacion,
+        Integer idEstadoHabitacion,
+        String estadoRegistro
+) {}
