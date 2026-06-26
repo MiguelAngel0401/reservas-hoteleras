8326c49d87d2172700505c6a4ea9c05dee816c33
heric-hh <erhh.015@gmail.com>
2026-06-26 08:21:57 -0600

    chore: limpiar .gitignore y remover archivos temporales y de configuración de IDE

--- commons/src/main/java/com/hotel/commons/dto/HuespedRequest.java
+++ commons/src/main/java/com/hotel/commons/dto/HuespedRequest.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public record HuespedRequest(
     @NotBlank(message = "El nombre es obligatorio")
-    @Size(min = 2, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
     String nombre,
     
     @NotBlank(message = "El apellido es obligatorio")
-    @Size(min = 2, max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
+    @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres")
     String apellido,
     
     @NotBlank(message = "El email es obligatorio")
